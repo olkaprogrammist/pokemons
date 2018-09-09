3a670f3a1803b4a1c56b48ea9ed348ed4623d6c1
osamokhvalova <@takeMehome987>
2018-09-09 22:42:40 +0300

    create full card component

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,11 +3,27 @@ import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { SearchCardComponent } from './components/search-card/search-card.component';
+import { RouterModule } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
+import * as fromSearchResults from './reducers/search.reducer';
+import { FullCardComponent } from './components/full-card/full-card.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: 'resource/:id',
+        component: FullCardComponent,
+      }
+    ]),
+    StoreModule.forFeature('search', fromSearchResults.reducer),
   ],
-  declarations: [SearchBarComponent, SearchResultsComponent, SearchCardComponent]
+  declarations: [
+    SearchBarComponent,
+    SearchResultsComponent,
+    SearchCardComponent,
+    FullCardComponent,
+  ]
 })
 export class SearchModule { }
